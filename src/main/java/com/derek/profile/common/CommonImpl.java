9bd2612d5a9e18c67b0ef08da2755275e96682b8
qux <qux@garena.com>
2017-08-19 12:16:58 +0800

    check when are beans created

--- src/main/java/com/derek/profile/common/CommonImpl.java
+++ src/main/java/com/derek/profile/common/CommonImpl.java
@@ -4,6 +4,11 @@ package com.derek.profile.common;
  * Created by qux on 12/8/17.
  */
 public class CommonImpl implements CommonClass {
+
+    public CommonImpl() {
+        System.out.println("Create CommonImpl");
+    }
+
     public void print() {
         System.out.println("I am common class");
     }
